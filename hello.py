400145c624314e144286b6b960754f7b8e0afd22
FuneralforaFriend <kkggol@naver.com>
2022-05-06 13:34:33 +0900

    yeardream algorithm completed!

--- hello.py
+++ hello.py
@@ -3,11 +3,9 @@
 # 과제 : 1 부터 10까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
 # 나머지 모든 경우는 숫자 그대로 출력
 for i in range(1, 21):
-    if (i % 3 == 0) & (i % 5 != 0):
-        print("year")
-    elif (i % 5 == 0) & (i % 3 != 0):
-        print("dream")
-    elif i % 15 == 0:
+    if i%15 == 0:
         print("yeardream")
-    else:
-        print(i)
+    elif i%3 == 0:
+        print("year")
+    elif i%5 == 0:
+        print("dream")
